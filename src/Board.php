c64c7992932cbc940e8052862e153577f7ad46e1
hojin <infohojin@gmail.com>
2019-04-02 23:42:51 +0900

    pagenation

--- src/Board.php
+++ src/Board.php
@@ -2,7 +2,37 @@
 
 namespace Jiny\Board;
 
-class Controllers
+class Board
 {
+    private $db;
+
+    public function __construct($db=null)
+    {
+        if($db) {
+            $this->db = $db;
+        } else {
+            // echo __DIR__;
+            // E:\test\jinydb\demo1\vendor\jiny\board\src
+            $dbconf = __DIR__."\..\..\..\..\conf\\"."dbconf.php";
+            $this->db = \Jiny\Database\db_init($dbconf);
+        }
+
+        $this->db->connect();      
+        
+    }
+
+    public function db()
+    {
+        return $this->db;
+    }
+
+    // 목록을 출력합니다.
+    public function list()
+    {
+
+    }
+
+
+    
 
 }
