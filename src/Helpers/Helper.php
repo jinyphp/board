5bb46ff9577abb7838da1ce79e4b7b686065232e
hojinlee <infohojin@gmail.com>
2019-10-30 09:33:06 +0900

    helper conf uridata

--- src/Helpers/Helper.php
+++ src/Helpers/Helper.php
@@ -15,7 +15,7 @@ if (! function_exists('id')) {
         if(isset($_POST['_id'])) {
             $id = $_POST['_id'];
         } else {
-            $arr = array_reverse(conf("req")['uridata']);
+            $arr = array_reverse(\jiny\conf("req")['uridata']);
             if (isset($arr[0]) && is_numeric($arr[0])) {
                 $id = intval($arr[0]);
             } else {
